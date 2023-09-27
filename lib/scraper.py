13016a913255a331a2b890ed5b3511fe9e802419
Joyce <joycewachira718@gmail.com>
2023-09-28 00:06:30 +0300

    Done

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,12 @@
-from turtle import ht
+# from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+for course in courses: 
+  print(course.contents[0].strip())
